20fd22acf3fb764b70d1b270426b627b03d8ae29
spindlymist <ocrobin@gmail.com>
2024-02-13 19:47:34 -0800

    Add footer with license details

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { createSignal } from "solid-js";
 import { EnemyList } from "./components/EnemyList";
 import "./App.css";
 
-function App() {
+export default function App() {
     const [filter, setFilter] = createSignal("");
 
     let input: HTMLInputElement|undefined;
@@ -11,15 +11,21 @@ function App() {
     };
 
     return (<>
-        <h1>Ikenfell Bestiary</h1>
-        <input
-            ref={input}
-            type="text"
-            onInput={updateFilter}
-            placeholder="Search"
-        />
-        <EnemyList filter={filter()} />
+        <header>
+            <h1>Ikenfell Bestiary</h1>
+        </header>
+        <main>
+            <input
+                ref={input}
+                type="text"
+                onInput={updateFilter}
+                placeholder="Search"
+            />
+            <EnemyList filter={filter()} />
+        </main>
+        <footer>
+            <p>The source code for this website is available under <a href="https://www.apache.org/licenses/LICENSE-2.0">the Apache-2.0 license.</a></p>
+            <p>All assets from Ikenfell, including but not limited to the background image, stat icons, enemy sprites, and item sprites, are &copy; Happy Ray Games. All rights reserved.</p>
+        </footer>
     </>);
 }
-
-export default App
